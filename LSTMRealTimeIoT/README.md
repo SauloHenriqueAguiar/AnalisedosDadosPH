9392b6139949c9756e598b45455cd27f29a12a2d
Saulo Henrique <78816148+SauloHenriqueAguiar@users.noreply.github.com>
2022-12-27 18:43:50 -0300

    Delete LSTMRealTimeIoT directory

--- LSTMRealTimeIoT/README.md
+++
@@ -1,4 +0,0 @@
-# Previsão de série temporal com LSTM em tempo real 
-
-sistema de previsão de dados em tempo real na edge.
-
--- LSTMRealTimeIoT/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# esta implementação do módulo principal lstm fornece uma implementação
-# de previsão de séries temporais usando uma abordagem lstm. é fornecido
-# como está sem garantias ou suporte.
-
-
-
-import warnings
-warnings.filterwarnings("ignore") # ignore messy numpy warnings
