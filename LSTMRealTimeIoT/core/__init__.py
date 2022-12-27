9342568292104a525128dc3419a06e5e654ca9aa
Saulo Henrique <78816148+SauloHenriqueAguiar@users.noreply.github.com>
2022-12-27 18:39:55 -0300

    Add files via upload

---
+++ LSTMRealTimeIoT/core/__init__.py
@@ -0,0 +1,8 @@
+# esta implementação do módulo principal lstm fornece uma implementação
+# de previsão de séries temporais usando uma abordagem lstm. é fornecido
+# como está sem garantias ou suporte.
+
+
+
+import warnings
+warnings.filterwarnings("ignore") # ignore messy numpy warnings
